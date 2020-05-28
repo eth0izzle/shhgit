5500a81ff3f22aaf4a7c890d38fdbdecc5f08498
Juho Saarinen <juho.saarinen@gmail.com>
2020-05-29 01:58:13 +0300

    Added support for running shhgit locally (#34)
    Added support to provide configuration file path as an parameter (helps if running within Docker)
    
    Updated local run documentation

--- core/config.go
+++ core/config.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path"
+	"path/filepath"
 	"strings"
 
 	"gopkg.in/yaml.v3"
@@ -27,18 +28,40 @@ type ConfigSignature struct {
 	Verifier string `yaml:"verifier,omitempty"`
 }
 
-func ParseConfig() (*Config, error) {
+func ParseConfig(options *Options) (*Config, error) {
 	config := &Config{}
+	var (
+		data []byte
+		err  error
+	)
 
-	dir, _ := os.Getwd()
-	data, err := ioutil.ReadFile(path.Join(dir, "config.yaml"))
-	if err != nil {
-		return config, err
+	if len(*options.ConfigPath) > 0 {
+		data, err = ioutil.ReadFile(path.Join(*options.ConfigPath, "config.yaml"))
+		if err != nil {
+			return config, err
+		}
+	} else {
+		// Trying to first find the configuration next to executable
+		// Helps e.g. with Drone where workdir is different than shhgit dir
+		ex, err := os.Executable()
+		dir := filepath.Dir(ex)
+		data, err = ioutil.ReadFile(path.Join(dir, "config.yaml"))
+		if err != nil {
+			dir, _ = os.Getwd()
+			data, err = ioutil.ReadFile(path.Join(dir, "config.yaml"))
+			if err != nil {
+				return config, err
+			}
+		}
 	}
 
 	err = yaml.Unmarshal(data, config)
 	if err != nil {
 		return config, err
+	}
+
+	if !options.LocalRun && (len(config.GitHubAccessTokens) < 1 || strings.TrimSpace(strings.Join(config.GitHubAccessTokens, "")) == "") {
+		return config, errors.New("You need to provide at least one GitHub Access Token. See https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line")
 	}
 
 	for i := 0; i < len(config.GitHubAccessTokens); i++ {
@@ -55,14 +78,11 @@ func ParseConfig() (*Config, error) {
 func (c *Config) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	*c = Config{}
 	type plain Config
+
 	err := unmarshal((*plain)(c))
 
 	if err != nil {
 		return err
-	}
-
-	if len(c.GitHubAccessTokens) < 1 || strings.TrimSpace(strings.Join(c.GitHubAccessTokens, "")) == "" {
-		return errors.New("You need to provide at least one GitHub Access Token. See https://help.github.com/en/articles/creating-a-personal-access-token-for-the-command-line")
 	}
 
 	return nil
